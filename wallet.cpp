67d5dfe81884d68f6a93e39bf6cf1f20647090fc
Ahmiyat <38369586+muhammadahmer1999@users.noreply.github.com>
2025-04-11 18:59:37 +0500

    wallet.cpp

---
+++ wallet.cpp
@@ -0,0 +1,21 @@
+#include "wallet.h"
+#include <openssl/ecdsa.h>
+#include <openssl/obj_mac.h>
+
+Wallet::Wallet() {
+    EC_KEY* key = EC_KEY_new_by_curve_name(NID_secp256k1);
+    EC_KEY_generate_key(key);
+
+    const EC_POINT* pubKey = EC_KEY_get0_public_key(key);
+    char* pubHex = EC_POINT_point2hex(EC_GROUP_new_by_curve_name(NID_secp256k1), 
+                                      pubKey, POINT_CONVERSION_UNCOMPRESSED, nullptr);
+    publicKey = string(pubHex);
+    OPENSSL_free(pubHex);
+
+    const BIGNUM* privKey = EC_KEY_get0_private_key(key);
+    char* privHex = BN_bn2hex(privKey);
+    privateKey = string(privHex);
+    OPENSSL_free(privHex);
+
+    EC_KEY_free(key);
+}
