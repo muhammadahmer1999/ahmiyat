43305ffa13b6d429e97ce0be5b26e7eb990ec67b
Ahmiyat <38369586+muhammadahmer1999@users.noreply.github.com>
2025-04-12 15:06:40 +0500

    utils.h

--- utils.h
+++ utils.h
@@ -2,11 +2,9 @@
 #define UTILS_H
 
 #include <string>
-#include <fstream> // Added for ofstream
-#include <iomanip> // Added for setw, setfill
 
 void log(const std::string& message);
-std::string uploadToIPFS(const std::string& filePath);
+std::string uploadToStorj(const std::string& filePath);  // Changed from uploadToIPFS
 std::string generateZKProof(const std::string& data);
 
 #endif
